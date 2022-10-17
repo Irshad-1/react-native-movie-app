f051aa4387077b0d622b26b97bb2fcdc926bd5c2
Md Irshad <md14015@gmail.com>
2022-10-17 18:01:58 +0530

    search result display done

--- App.js
+++ App.js
@@ -19,6 +19,8 @@ import {
   View,
   Image,
   FlatList,
+  TouchableHighlight,
+  Pressable,
 } from 'react-native';
 
 import {
@@ -31,37 +33,14 @@ import {
 
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const [text, setText] = React.useState('');
+  const [display, setDisplay] = React.useState(false);
+  const [text, onChangeText] = React.useState('');
   const [data, setData] = React.useState({});
+  const [searchData, setSearchData] = React.useState([]);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -73,10 +52,12 @@ const App = () => {
     res = await res.json();
     console.log('res', res);
     setData(res);
+    setDisplay(false);
+    onChangeText('');
   };
   let timer;
   React.useEffect(() => {
-    timer = setTimeout(searchMovieByLetters, 4000);
+    timer = setTimeout(searchMovieByLetters, 2000);
     return () => clearTimeout(timer);
   }, [text]);
   const searchMovieByLetters = async () => {
@@ -84,21 +65,39 @@ const App = () => {
     let res = await fetch(
       `https://www.omdbapi.com/?&apikey=c82dc22a&s=${text}`,
     );
+
     res = await res.json();
+    if (res.Search) setDisplay(true);
     console.log(res.Search);
     setSearchData(res.Search);
   };
 
   const SearchDataDisplay = props => {
     return (
-      <View>
-        <Text>{props.item.Title}</Text>
-        <Image
-          source={{uri: props.item.Poster}}
-          style={{width: 50, height: 50}}
-          resizeMode="contain"
-        />
-      </View>
+      <Pressable
+        onPress={() => {
+          setDisplay(false);
+          onChangeText(props.item.title);
+          searchMovie();
+        }}
+        underlayColor="white">
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            flex: 1,
+            borderWidth: 0.5,
+            borderColor: 'grey',
+          }}>
+          <Text style={{flex: 2}}>{props.item.Title}</Text>
+          <Image
+            source={{uri: props.item.Poster}}
+            style={{width: 80, height: 80, flex: 0.5}}
+            resizeMode="contain"
+          />
+        </View>
+      </Pressable>
     );
   };
 
@@ -106,12 +105,15 @@ const App = () => {
     <SafeAreaView
       contentInsetAdjustmentBehavior="automatic"
       style={backgroundStyle}>
-      <Header />
       <View
         style={{
           backgroundColor: isDarkMode ? Colors.black : Colors.white,
         }}>
+        <Text style={{textAlign: 'center', fontSize: 40, fontWeight: 'bold'}}>
+          IMDB
+        </Text>
         <TextInput
+          style={{fontSize: 25}}
           value={text}
           onChangeText={te => {
             console.log('te', te);
@@ -125,13 +127,24 @@ const App = () => {
           style={{width: 400, height: 500}}
           resizeMode="contain"
         />
-        <FlatList
-          data={searchData}
-          style={{height: 400, position: 'absolute', top: 50}}
-          renderItem={({item}) => {
-            return <SearchDataDisplay key={item.key} item={item} />;
-          }}
-        />
+        {console.log(display)}
+
+        {display && (
+          <FlatList
+            data={searchData}
+            style={{
+              height: 300,
+              position: 'absolute',
+              top: 105,
+              width: '95%',
+              elevation: 2,
+              backgroundColor: 'white',
+            }}
+            renderItem={({item}) => {
+              return <SearchDataDisplay key={item.key} item={item} />;
+            }}
+          />
+        )}
       </View>
     </SafeAreaView>
   );
